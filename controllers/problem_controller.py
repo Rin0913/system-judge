603816de11fb2280ca985c1b6ec4323dc58e0890
DengRoger <rogerdeng92@gmail.com>
2024-10-14 16:08:52 +0000

    Add initial project files and configurations

---
+++ controllers/problem_controller.py
@@ -0,0 +1,34 @@
+from flask import jsonify, request, g, Blueprint  # Third-party imports
+
+problem_bp = Blueprint('problem', __name__)
+
+@problem_bp.route('/create', methods=['POST'])
+def create_problem():
+    problem_id = g.problem_repository.create_problem()
+    return jsonify({'problem_id': problem_id})
+
+@problem_bp.route('/update/<string:problem_id>', methods=['PUT'])
+def update_problem(problem_id):
+    problem_name = request.json.get('problem_name')
+    start_time = request.json.get('start_time')
+    deadline = request.json.get('deadline')
+    problem_id = g.problem_repository.update_problem_details(problem_id,
+                                                             problem_name,
+                                                             start_time,
+                                                             deadline)
+    return jsonify({'problem_id': problem_id})
+
+@problem_bp.route('/<string:problem_id>', methods=['GET'])
+def query_problem(problem_id):
+    problem_data = g.problem_repository.query_problem(problem_id)
+    return jsonify(problem_data)
+
+@problem_bp.route('/list', methods=['GET'])
+def list_problems():
+    problem_data = g.problem_repository.list_problems()
+    return jsonify(problem_data)
+
+@problem_bp.route('/<string:problem_id>', methods=['DELETE'])
+def del_problem(problem_id):
+    problem_id = g.problem_repository.del_problem(problem_id)
+    return jsonify({'problem_id': problem_id})
---
+++ init.sql
@@ -0,0 +1,71 @@
+USE `system_judge`;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    role VARCHAR(255) DEFAULT 'user'
+);
+
+CREATE TABLE problems (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    is_valid BOOLEAN DEFAULT TRUE,
+    allow_submission BOOLEAN DEFAULT FALSE,
+    problem_name VARCHAR(255),
+    created_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    start_time DATETIME,
+    deadline DATETIME
+);
+
+CREATE TABLE submissions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    problem_id INT,
+    submission_score INT,
+    submit_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (problem_id) REFERENCES problems(id)
+);
+
+CREATE TABLE subtasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    problem_id INT,
+    task_name VARCHAR(255),
+    points INT,
+    is_valid BOOLEAN DEFAULT TRUE,
+    FOREIGN KEY (problem_id) REFERENCES problems(id)
+);
+
+CREATE TABLE wireguard_profiles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    is_valid BOOLEAN DEFAULT TRUE,
+    creation_date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE subtask_results (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    submission_id INT,
+    task_id INT,
+    is_passed BOOLEAN,
+    points INT,
+    FOREIGN KEY (submission_id) REFERENCES submissions(id),
+    FOREIGN KEY (task_id) REFERENCES subtasks(id)
+);
+
+CREATE TABLE subtask_dependencies(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    parent_task_id INT,
+    child_task_id INT,
+    FOREIGN KEY (parent_task_id) REFERENCES subtasks(id),
+    FOREIGN KEY (child_task_id) REFERENCES subtasks(id)
+);
+
+CREATE TABLE subtask_playbooks(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    is_valid BOOLEAN DEFAULT TRUE,
+    problem_id INT,
+    playbook_name VARCHAR(255),
+    FOREIGN KEY (problem_id) REFERENCES problems(id)
+);
