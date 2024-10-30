910ed32e4b25a6c5c93b494513e3346254c72c46
K Rin <rin@sandb0x.tw>
2024-10-30 22:50:48 +0800

    Add function document for auth_controller login.

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -4,6 +4,14 @@ auth_bp = Blueprint('auth', __name__)
 
 @auth_bp.route('/login', methods=['POST'])
 def login():
+    """
+    the format of profile would be like:
+    {
+        "uid": username,
+        "role": role
+    }
+    and the role would be 'admin' or 'user'.
+    """
     username = request.json.get('username', 'nobody')
     password = request.json.get('password', '')
     profile = current_app.ldap_service.authenticate(username, password)
