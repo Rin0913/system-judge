56865cff49db6cf28dddb68997ed56f7edb5b9ef
K Rin <rin@sandb0x.tw>
2024-11-08 05:43:23 +0800

    Improve user experience for the cooldown reseting util.

--- utils/reset_cooldown_time.sh
+++ utils/reset_cooldown_time.sh
@@ -3,4 +3,4 @@
 current_dir=$(dirname "$(realpath $0)")
 python_dir=$(dirname "$current_dir")
 
-PYTHONPATH=$python_dir python3 $python_dir/utils/reset_cooldown_time.py -u $1 -p $2
+PYTHONPATH=$python_dir python3 $python_dir/utils/reset_cooldown_time.py $@
